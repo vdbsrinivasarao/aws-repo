fd91f7e777d78349c4455ef2b78c3299c729782e
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-27 20:00:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "vazralasg"
+  default     = "vazralaterrasg"
 }
 
 variable "tag_name" {
