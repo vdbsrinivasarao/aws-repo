83ede381d2a01c110de5e704ddb143a9cde35e0d
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-27 21:46:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "security_group" {
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+  default     = "Terra-ec2-instance"
 }
 
 variable "ami_id" {
